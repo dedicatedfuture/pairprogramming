8a8b3156ffdfd1da57406f9378a4ac59eaac7ab0
William Zerega <Bill@Williams-MacBook-Pro.local>
2016-04-20 16:19:26 -0400

    come on sendgrid

--- app/mailers/appointment_mailer.rb
+++ app/mailers/appointment_mailer.rb
@@ -3,8 +3,8 @@ class AppointmentMailer < ActionMailer::Base
   default :from => 'no-reply@example.com',
           :subject => 'An email sent via SendGrid with substitutions'
 
-  def confirmation_email
+  def confirmation_email(email)
     category 'SendGridRocks'
-    mail :to => 'zerega85@gmail.com'
+    mail :to => email
   end
 end
